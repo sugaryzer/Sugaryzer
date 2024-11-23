b0ad6e76da47acbe767541a1bb25d26486f81e5a
Renaldyp <37020760+renaldypermana@users.noreply.github.com>
2024-11-23 23:44:35 +0700

    feat: change to fragment list

--- app/src/main/java/com/sugaryzer/sugaryzer/SectionsPagerAdapter.kt
+++ app/src/main/java/com/sugaryzer/sugaryzer/SectionsPagerAdapter.kt
@@ -5,20 +5,17 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.sugaryzer.sugaryzer.ui.history.HistoryFragment
 import com.sugaryzer.sugaryzer.ui.home.HomeFragment
+import com.sugaryzer.sugaryzer.ui.news.NewsFragment
 import com.sugaryzer.sugaryzer.ui.profile.ProfileFragment
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    private val fragments = listOf(HomeFragment(), HistoryFragment(), NewsFragment(), ProfileFragment())
+
     override fun getItemCount(): Int {
-        return 3
+        return fragments.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = HistoryFragment()
-            2 -> fragment = ProfileFragment()
-        }
-        return fragment as Fragment
+        return fragments[position]
     }
 }
