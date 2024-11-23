d0c90cb77a4a03bb2a7bb5b8377414c2a1aefb4d
Renaldyp <37020760+renaldypermana@users.noreply.github.com>
2024-11-23 23:44:20 +0700

    feat: modify mainActivity

--- app/src/main/java/com/sugaryzer/sugaryzer/MainActivity.kt
+++ app/src/main/java/com/sugaryzer/sugaryzer/MainActivity.kt
@@ -1,67 +1,46 @@
 package com.sugaryzer.sugaryzer
 
 import android.os.Bundle
-import androidx.annotation.StringRes
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-import com.sugaryzer.sugaryzer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewPager: ViewPager2
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var adapter: SectionsPagerAdapter
 
-    companion object {
-        @StringRes
-        private val TAB_TITLES = intArrayOf(
-            R.string.title_home,
-            R.string.title_history,
-            R.string.title_profile
-        )
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this)
-        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
+        viewPager = findViewById(R.id.viewPager)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
 
-        TabLayoutMediator(tabs, viewPager) { tab, position ->
-            tab.text = when (position) {
-                0 -> "Home"
-                1 -> "History"
-                2 -> "Profile"
-                else -> null
+        adapter = SectionsPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> viewPager.currentItem = 0
+                R.id.navigation_history -> viewPager.currentItem = 1
+                R.id.navigation_news -> viewPager.currentItem = 2
+                R.id.navigation_profile -> viewPager.currentItem = 3
             }
-        }.attach()
-//        TabLayoutMediator(tabs, viewPager) { tab, position ->
-//            tab.text = resources.getString(TAB_TITLES[position])
-//        }.attach()
-//        supportActionBar?.elevation = 0f
+            true
+        }
 
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        val navView: BottomNavigationView = binding.navView
-//
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> bottomNavigationView.selectedItemId = R.id.navigation_home
+                    1 -> bottomNavigationView.selectedItemId = R.id.navigation_history
+                    2 -> bottomNavigationView.selectedItemId = R.id.navigation_news
+                    3 -> bottomNavigationView.selectedItemId = R.id.navigation_profile
+                }
+            }
+        })
     }
 }
