f40f6f80377182d572e590dd80479acd73137162
Renaldyp <renaldypermana7@gmail.com>
2024-11-24 20:11:54 +0700

    feat: add edit profile

--- app/src/main/java/com/sugaryzer/sugaryzer/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/sugaryzer/sugaryzer/ui/profile/ProfileFragment.kt
@@ -1,31 +1,37 @@
 package com.sugaryzer.sugaryzer.ui.profile
 
-import androidx.fragment.app.viewModels
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.lifecycle.ViewModelProvider
 import com.sugaryzer.sugaryzer.R
+import com.sugaryzer.sugaryzer.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
-    private val viewModel: ProfileViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding =  FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        val editProfile: LinearLayout = view.findViewById(R.id.editAccount)
+        editProfile.setOnClickListener{
+            val intent = Intent(requireContext(), EditProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
