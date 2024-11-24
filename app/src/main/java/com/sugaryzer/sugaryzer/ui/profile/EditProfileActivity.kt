f0e6d11d202b25d920a2c09532b57f615696b4c3
Renaldyp <renaldypermana7@gmail.com>
2024-11-24 20:13:25 +0700

    feat: add edit profile activity

---
+++ app/src/main/java/com/sugaryzer/sugaryzer/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.sugaryzer.sugaryzer.ui.profile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sugaryzer.sugaryzer.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
