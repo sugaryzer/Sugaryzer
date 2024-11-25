fcb08d2caab5b5e3e84a6df1971c1830f94c3ab3
prdptbgs <rerickku@gmail.com>
2024-11-25 14:03:06 +0700

    Add SignInFragment and SignUpFragment with integration in AuthActivity
    - Added SignInFragment and SignUpFragment to handle user authentication flow.
    - Integrated both fragments into AuthActivity as the main host for fragment navigation.
    - Default fragment loaded is SignInFragment when AuthActivity is started.

--- app/src/main/java/com/sugaryzer/sugaryzer/SplashScreen.kt
+++ app/src/main/java/com/sugaryzer/sugaryzer/SplashScreen.kt
@@ -3,8 +3,11 @@ package com.sugaryzer.sugaryzer
 import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.sugaryzer.sugaryzer.ui.main.MainActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sugaryzer.sugaryzer.ui.login.AuthActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +19,7 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
         android.os.Handler().postDelayed(
             {
-                val intent = Intent(this, MainActivity::class.java)
+                val intent = Intent(this, AuthActivity::class.java)
                 startActivity(intent)
                 finish()
             },
