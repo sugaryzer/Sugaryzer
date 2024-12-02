6543b75a7814a2e95cfc84df15e3df02162448b6
Renaldyp <renaldypermana7@gmail.com>
2024-12-02 14:10:20 +0700

    feat: add floating button

--- app/src/main/java/com/sugaryzer/sugaryzer/SplashScreen.kt
+++ app/src/main/java/com/sugaryzer/sugaryzer/SplashScreen.kt
@@ -3,11 +3,8 @@ package com.sugaryzer.sugaryzer
 import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.sugaryzer.sugaryzer.ui.login.AuthActivity
+import com.sugaryzer.sugaryzer.ui.auth.AuthActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
