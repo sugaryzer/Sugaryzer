ae2920c0d50c8a3252c1e8f665112192d89d459d
Renaldyp <renaldypermana7@gmail.com>
2024-12-02 14:10:41 +0700

    feat: add viewmodel to factory

--- app/src/main/java/com/sugaryzer/sugaryzer/ViewModelFactory.kt
+++ app/src/main/java/com/sugaryzer/sugaryzer/ViewModelFactory.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.sugaryzer.sugaryzer.data.di.Injection
 import com.sugaryzer.sugaryzer.data.repository.SugaryzerRepository
+import com.sugaryzer.sugaryzer.ui.auth.AuthViewModel
+import com.sugaryzer.sugaryzer.ui.auth.signin.SignInFragment
+import com.sugaryzer.sugaryzer.ui.auth.signin.SignInViewModel
+import com.sugaryzer.sugaryzer.ui.auth.signup.SignUpViewModel
 import com.sugaryzer.sugaryzer.ui.profile.ProfileViewModel
 
 class ViewModelFactory(
@@ -16,6 +20,15 @@ class ViewModelFactory(
         return when {
             modelClass.isAssignableFrom(ProfileViewModel::class.java) -> {
                 ProfileViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(AuthViewModel::class.java) -> {
+                AuthViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(SignInViewModel::class.java) -> {
+                SignInViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
+                SignUpViewModel(repository) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
         }
