0d77d6df3df9626477b5088f481d9711277a231c
Renaldyp <renaldypermana7@gmail.com>
2024-12-04 14:52:30 +0700

    remove: unused fragment

--- app/src/main/java/com/sugaryzer/sugaryzer/ui/auth/signup/SignUpViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.sugaryzer.sugaryzer.ui.auth.signup
-
-import androidx.lifecycle.ViewModel
-import com.sugaryzer.sugaryzer.data.repository.SugaryzerRepository
-
-class SignUpViewModel(private val repository: SugaryzerRepository) : ViewModel() {
-    // TODO: Implement the ViewModel
-}
