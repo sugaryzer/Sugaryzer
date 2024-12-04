4300e2b1644c2211ad9e453f8937b6483542819a
Renaldyp <renaldypermana7@gmail.com>
2024-12-04 14:51:52 +0700

    feat: add sign up and sign in activity

---
+++ app/src/main/java/com/sugaryzer/sugaryzer/ui/signin/SignInViewModel.kt
@@ -0,0 +1,37 @@
+package com.sugaryzer.sugaryzer.ui.signin
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sugaryzer.sugaryzer.data.ResultState
+import com.sugaryzer.sugaryzer.data.repository.SugaryzerRepository
+import com.sugaryzer.sugaryzer.data.response.LoginResponse
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import retrofit2.HttpException
+
+class SignInViewModel(private val repository: SugaryzerRepository) : ViewModel() {
+    private val _responseResult = MutableLiveData<ResultState<LoginResponse>>()
+    val responseResult = _responseResult
+
+    fun signin(email:String, password:String){
+        viewModelScope.launch {
+            try {
+                _responseResult.value = ResultState.Loading
+                val response = repository.login(email,password)
+                if(response.result.accessToken.isNotEmpty()){
+                    repository.saveSession(response.result.accessToken)
+                    _responseResult.value = ResultState.Success(response)
+                }
+            } catch (e: HttpException){
+                val errorBody = e.response()?.errorBody()?.string()
+                val errorMessage = try {
+                    JSONObject(errorBody).getString("message")
+                } catch (jsonException: Exception) {
+                    errorBody ?: e.message ?: "Unknown error occurred"
+                }
+                _responseResult.value = ResultState.Error(errorMessage)
+            }
+        }
+    }
+}
