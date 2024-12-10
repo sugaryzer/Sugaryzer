7aff47ff2b7369bf7b52f00f0f57eeb9c11728e5
Renaldyp <renaldypermana7@gmail.com>
2024-12-10 14:38:02 +0700

    feat: modify scan feature and add scan function

--- app/src/main/java/com/sugaryzer/sugaryzer/ui/scan/ConfirmActivity.kt
+++ app/src/main/java/com/sugaryzer/sugaryzer/ui/scan/ConfirmActivity.kt
@@ -1,17 +1,26 @@
 package com.sugaryzer.sugaryzer.ui.scan
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sugaryzer.sugaryzer.R
 import com.sugaryzer.sugaryzer.ViewModelFactory
+import com.sugaryzer.sugaryzer.data.ResultState
+import com.sugaryzer.sugaryzer.data.helper.reduceFileImage
+import com.sugaryzer.sugaryzer.data.helper.uriToFile
 import com.sugaryzer.sugaryzer.databinding.ActivityConfirmBinding
+import com.sugaryzer.sugaryzer.ui.profile.EditProfileActivity
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
 
 class ConfirmActivity : AppCompatActivity() {
 
@@ -35,11 +44,58 @@ class ConfirmActivity : AppCompatActivity() {
             Log.e("Image URI", "URI is null!")
         }
 
+        val imageFile = uriToFile(imageUri!!, this)
+        val compressedImage = imageFile.reduceFileImage()
+        val photoRequestBody = compressedImage.asRequestBody("image/jpeg".toMediaTypeOrNull())
+        val photoMultipart =
+            MultipartBody.Part.createFormData(
+                "file",
+                compressedImage.name,
+                photoRequestBody
+            )
+
         binding.btnBack.setOnClickListener{
             finish()
         }
         binding.btnConfirm.setOnClickListener {
-            Toast.makeText(this, "Image confirmed", Toast.LENGTH_SHORT).show()
+            viewModel.uploadStory(photoMultipart)
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setView(R.layout.loading)
+            val dialog: AlertDialog = builder.create()
+            viewModel.uploadResult.observe(this){ response ->
+                when (response) {
+                    is ResultState.Loading -> dialog.show()
+                    is ResultState.Error -> {
+                        dialog.dismiss()
+                        AlertDialog.Builder(this).apply {
+                            setTitle("Gagal mendeteksi gambar")
+                            setMessage(response.message)
+                            create()
+                            show()
+                        }
+                    }
+
+                    is ResultState.Success -> {
+                        dialog.dismiss()
+                        val intent = Intent(this, ProductInformationActivity::class.java).apply {
+                            putExtra(ProductInformationActivity.EXTRA_PRODUCT_ID, response.data.result.id)
+                            putExtra(ProductInformationActivity.EXTRA_PRODUCT_CODE, response.data.result.code)
+                            putExtra(ProductInformationActivity.EXTRA_PRODUCT_NAME, response.data.result.name)
+                            putExtra(ProductInformationActivity.EXTRA_PRODUCT_SUGAR, response.data.result.amountOfSugar)
+                        }
+                        startActivity(intent)
+                    }
+
+                    else -> dialog.dismiss()
+                }
+
+            }
+//            val intent = Intent(this, ProductInformationActivity::class.java).apply {
+//                putExtra(ProductInformationActivity.EXTRA_PRODUCT_ID, 3)
+//                putExtra(ProductInformationActivity.EXTRA_PRODUCT_NAME, "Fibe Mini Botol 100 ml")
+//                putExtra(ProductInformationActivity.EXTRA_PRODUCT_SUGAR, 9)
+//            }
+//            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/sugaryzer/sugaryzer/ui/scan/ScanVIewModel.kt
+++ app/src/main/java/com/sugaryzer/sugaryzer/ui/scan/ScanVIewModel.kt
@@ -4,16 +4,23 @@ import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sugaryzer.sugaryzer.data.ResultState
 import com.sugaryzer.sugaryzer.data.repository.SugaryzerRepository
-import com.sugaryzer.sugaryzer.data.response.MessageResponse
+import com.sugaryzer.sugaryzer.data.response.LoginResponse
+import com.sugaryzer.sugaryzer.data.response.ScanResponse
+import com.sugaryzer.sugaryzer.data.response.ScannedResponse
+import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
+import org.json.JSONObject
+import retrofit2.HttpException
 
 class ScanViewModel(private val sugaryzerRepository: SugaryzerRepository): ViewModel() {
     private val _imageUri = MutableLiveData<Uri?>()
     val imageUri: LiveData<Uri?> = _imageUri
 
-    private val _uploadResult = MutableLiveData<ResultState<MessageResponse>>()
-    val uploadResult: LiveData<ResultState<MessageResponse>> = _uploadResult
+    private val _uploadResult = MutableLiveData<ResultState<ScanResponse>>()
+    val uploadResult: LiveData<ResultState<ScanResponse>> = _uploadResult
 
 
     fun setImageUri(uri: Uri) {
@@ -22,5 +29,41 @@ class ScanViewModel(private val sugaryzerRepository: SugaryzerRepository): ViewM
 
     fun getImageUri(): Uri? {
         return _imageUri.value
+    }
+
+    fun uploadStory(
+        photo: MultipartBody.Part,
+    ) {
+        viewModelScope.launch {
+            _uploadResult.value = ResultState.Loading
+            try {
+                val response = sugaryzerRepository.scanImage(photo)
+
+                _uploadResult.value = response
+            } catch (e: Exception) {
+                _uploadResult.value = ResultState.Error("An unexpected error occurred: ${e.message}")
+            }
+        }
+    }
+
+    private val _scannedResult = MutableLiveData<ResultState<ScannedResponse>>()
+    val scannedResult = _scannedResult
+
+    fun uploadScan(code:String, sugarConsume:Double){
+        viewModelScope.launch {
+            try {
+                _scannedResult.value = ResultState.Loading
+                val response = sugaryzerRepository.uploadScan(code,sugarConsume)
+                _scannedResult.value = ResultState.Success(response)
+            } catch (e: HttpException){
+                val errorBody = e.response()?.errorBody()?.string()
+                val errorMessage = try {
+                    JSONObject(errorBody).getString("message")
+                } catch (jsonException: Exception) {
+                    errorBody ?: e.message ?: "Unknown error occurred"
+                }
+                _scannedResult.value = ResultState.Error(errorMessage)
+            }
+        }
     }
 }
